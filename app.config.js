e52f094e8cf093bb768031685b19ff5223021b84
JW <5948+funkfinger@users.noreply.github.com>
2025-04-22 09:35:44 -0700

    Enable React Native New Architecture in app.config.js

--- app.config.js
+++ app.config.js
@@ -1,41 +1,42 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   expo: {
-    name: 'deworm-chatbot',
-    slug: 'deworm-chatbot',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/images/icon.png',
-    scheme: 'deworm',
-    userInterfaceStyle: 'automatic',
+    name: "deworm-chatbot",
+    slug: "deworm-chatbot",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "deworm",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
     splash: {
-      image: './assets/images/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff'
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    assetBundlePatterns: ['**/*'],
+    assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.funkfinger.deworm'
+      bundleIdentifier: "com.funkfinger.deworm",
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: './assets/images/adaptive-icon.png',
-        backgroundColor: '#ffffff'
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      package: 'com.funkfinger.deworm'
+      package: "com.funkfinger.deworm",
     },
     web: {
-      bundler: 'metro',
-      favicon: './assets/images/favicon.png'
+      bundler: "metro",
+      favicon: "./assets/images/favicon.png",
     },
     extra: {
       SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
       SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
       eas: {
-        projectId: 'your-project-id'
-      }
-    }
-  }
+        projectId: "your-project-id",
+      },
+    },
+  },
 };
